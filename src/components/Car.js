db9f49eb103d865e579d1acdd6bbbd81b4679e2b
mayrant <mayrant2004@yahoo.com>
2021-09-24 02:58:18 -0500

    imported mui to style car component, project complete

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import cars from '../cars.json';
 // import material ui components here //
-// Container, Paper, Chip //
+import { Container } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+import { Chip } from '@material-ui/core';
 
 const Car = (props) => {
   const id = props.match.params.id;
-  const carFound = cars.find(car => car.id == id)  
+  const carFound = cars.find((car) => car.id == id);
   return (
-    <h2>{carFound.Name}</h2>
+    <Container maxWidth="sm" className="car-container">
+      <Paper className="car-paper">
+        <h2>{carFound.Name}</h2>
+        {Object.entries(carFound).map(([key, value]) => {
+            return <Chip label={`${key}: ${value}`} />
+        })}
+      </Paper>
+    </Container>
   );
 };
 
