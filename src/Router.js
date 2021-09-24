dfceb034a3e880ceed17c240e77e90f992610327
mayrant <mayrant2004@yahoo.com>
2021-09-24 00:31:58 -0500

    created router and imported it to app

--- src/Router.js
+++ src/Router.js
@@ -10,8 +10,19 @@ Remember to export the component at the end
 
 import React from 'react'
 import { Switch, Route } from 'react-router'
+
 // Write component imports here //
-
-
+import Home from '../src/components/Home'
+import About from '../src/components/About'
 
 // Start Router function here //
+const Router = () => {
+    return (
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+        </Switch>
+    )
+}
+
+export default Router;
